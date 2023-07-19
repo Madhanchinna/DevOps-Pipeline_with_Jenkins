85f4671e09dc06e62a3b088b901b57fa34eafd10
Madhanchinna <122683475+Madhanchinna@users.noreply.github.com>
2023-07-19 19:20:06 +0530

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven > /tmp/.auth
 echo $PASS >> /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 
-scp -i /opt/prod.ppk /tmp/.auth kavi@18.189.189.179:/tmp/.auth
-scp -i /opt/prod.ppk ./jenkins/deploy/publish kavi@18.189.189.179:/tmp/publish
-ssh -i /opt/prod.ppk kavi@18.189.189.179 "/tmp/publish"
+scp -i /opt/prod.ppk /tmp/.auth <remote_user>@<remote_machine_IP>:/tmp/.auth
+scp -i /opt/prod.ppk ./jenkins/deploy/publish <remote_user>@<remote_machine_IP>:/tmp/publish
+ssh -i /opt/prod.ppk <remote_user>@<remote_machine_IP> "/tmp/publish"
